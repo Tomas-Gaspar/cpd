c165e7aa7cf1f08a9057cf75ff40cf36ea0c304f
Pedro <89313201+pramadureira@users.noreply.github.com>
2024-03-15 20:20:42 +0000

    Improved the block algorithm for blocks not multiples of the matrix size

--- assign1/src/auxi.cpp
+++ assign1/src/auxi.cpp
@@ -3,18 +3,14 @@
 #include <iomanip>
 #include <time.h>
 #include <cstdlib>
-#include <papi.h>
 #include <omp.h>
 
 using namespace std;
 
 #define SYSTEMTIME clock_t
 
- 
 void OnMult(int m_ar, int m_br) 
 {
-    
-    SYSTEMTIME Time1, Time2;
     
     char st[100];
     double temp;
@@ -39,9 +35,6 @@ void OnMult(int m_ar, int m_br)
             phb[i*m_br + j] = (double)(i+1);
 
 
-
-    Time1 = clock();
-
     for(i=0; i<m_ar; i++)
     {    for( j=0; j<m_br; j++)
         {    temp = 0;
@@ -53,9 +46,6 @@ void OnMult(int m_ar, int m_br)
         }
     }
 
-
-    Time2 = clock();
-    sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
     cout << st;
 
     // display 10 elements of the result matrix tto verify correctness
@@ -74,8 +64,6 @@ void OnMult(int m_ar, int m_br)
 
 void OnMultLine(int m_ar, int m_br)
 {
-
-    SYSTEMTIME Time1, Time2;
     
     char st[100];
     double temp;
@@ -102,8 +90,6 @@ void OnMultLine(int m_ar, int m_br)
             phc[i*m_br + j] = (double)0.0;
 
 
-    Time1 = clock();
-
     for (i = 0; i < m_ar; i++) {
         for (k = 0; k < m_ar; k++) {
             for (j = 0; j < m_ar; j++) {
@@ -111,10 +97,6 @@ void OnMultLine(int m_ar, int m_br)
             }
         }
     }
-
-
-    Time2 = clock();
-    sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
     cout << st;
 
     // display 10 elements of the result matrix tto verify correctness
@@ -138,7 +120,6 @@ int min(int a, int b) {
 // add code here for block x block matriz multiplication
 void OnMultBlock(int m_ar, int m_br, int bkSize)
 {
-    SYSTEMTIME Time1, Time2;
     
     char st[100];
     double temp;
@@ -162,7 +143,6 @@ void OnMultBlock(int m_ar, int m_br, int bkSize)
         for(j=0; j<m_br; j++)
             phc[i*m_br + j] = (double)0.0;
 
-	Time1 = clock();
     
     for (ii = 0; ii < m_ar; ii+=bkSize) {
 		for (kk = 0; kk < m_ar; kk+=bkSize) {
@@ -181,9 +161,6 @@ void OnMultBlock(int m_ar, int m_br, int bkSize)
             }
         }
     }
-
-	Time2 = clock();
-	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
 	cout << st;
 
     // display 10 elements of the result matrix to verify correctness
@@ -202,9 +179,6 @@ void OnMultBlock(int m_ar, int m_br, int bkSize)
 
 void OnMultLineParallel1(int m_ar, int m_br)
 {
-
-    double Time1, Time2;
-    
     char st[100];
     double temp;
 
@@ -229,8 +203,6 @@ void OnMultLineParallel1(int m_ar, int m_br)
             phc[i*m_br + j] = (double)0.0;
 
 
-    Time1 = omp_get_wtime();
-
     #pragma omp parallel for
     for (int i = 0; i < m_ar; i++) {
         for (int k = 0; k < m_ar; k++) {
@@ -240,9 +212,6 @@ void OnMultLineParallel1(int m_ar, int m_br)
         }
     }
 
-
-    Time2 = omp_get_wtime();
-    sprintf(st, "Time: %3.3f seconds\n", Time2 - Time1);
     cout << st;
 
     // display 10 elements of the result matrix tto verify correctness
@@ -261,8 +230,6 @@ void OnMultLineParallel1(int m_ar, int m_br)
 
 void OnMultLineParallel2(int m_ar, int m_br)
 {
-
-    double Time1, Time2;
     
     char st[100];
     double temp;
@@ -288,8 +255,6 @@ void OnMultLineParallel2(int m_ar, int m_br)
             phc[i*m_br + j] = (double)0.0;
 
 
-    Time1 = omp_get_wtime();
-
     #pragma omp parallel
     for (int i = 0; i < m_ar; i++) {
         for (int k = 0; k < m_ar; k++) {
@@ -301,8 +266,6 @@ void OnMultLineParallel2(int m_ar, int m_br)
     }
 
 
-    Time2 = omp_get_wtime();
-    sprintf(st, "Time: %3.3f seconds\n", Time2 - Time1);
     cout << st;
 
     // display 10 elements of the result matrix tto verify correctness
@@ -321,25 +284,6 @@ void OnMultLineParallel2(int m_ar, int m_br)
 
 
 
-void handle_error (int retval)
-{
-  printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
-  exit(1);
-}
-
-void init_papi() {
-  int retval = PAPI_library_init(PAPI_VER_CURRENT);
-  if (retval != PAPI_VER_CURRENT && retval < 0) {
-    printf("PAPI library version mismatch!\n");
-    exit(1);
-  }
-  if (retval < 0) handle_error(retval);
-
-  std::cout << "PAPI Version Number: MAJOR: " << PAPI_VERSION_MAJOR(retval)
-            << " MINOR: " << PAPI_VERSION_MINOR(retval)
-            << " REVISION: " << PAPI_VERSION_REVISION(retval) << "\n";
-}
-
 
 int main (int argc, char *argv[])
 {
@@ -348,28 +292,7 @@ int main (int argc, char *argv[])
 	int lin, col, blockSize;
 	int op;
 	
-	int EventSet = PAPI_NULL;
-  	long long values[2];
-  	int ret;
 	
-
-	ret = PAPI_library_init( PAPI_VER_CURRENT );
-	if ( ret != PAPI_VER_CURRENT )
-		std::cout << "FAIL" << endl;
-
-
-	ret = PAPI_create_eventset(&EventSet);
-		if (ret != PAPI_OK) cout << "ERROR: create eventset" << endl;
-
-
-	ret = PAPI_add_event(EventSet,PAPI_L1_DCM );
-	if (ret != PAPI_OK) cout << "ERROR: PAPI_L1_DCM" << endl;
-
-
-	ret = PAPI_add_event(EventSet,PAPI_L2_DCM);
-	if (ret != PAPI_OK) cout << "ERROR: PAPI_L2_DCM" << endl;
-
-
 	op=1;
 	do {
 		cout << endl << "1. Multiplication" << endl;
@@ -385,10 +308,6 @@ int main (int argc, char *argv[])
    		cin >> lin;
    		col = lin;
 
-
-		// Start counting
-		ret = PAPI_start(EventSet);
-		if (ret != PAPI_OK) cout << "ERROR: Start PAPI" << endl;
 
 		switch (op){
 			case 1: 
@@ -411,29 +330,5 @@ int main (int argc, char *argv[])
 
 		}
 
-  		ret = PAPI_stop(EventSet, values);
-  		if (ret != PAPI_OK) cout << "ERROR: Stop PAPI" << endl;
-  		printf("L1 DCM: %lld \n",values[0]);
-  		printf("L2 DCM: %lld \n",values[1]);
-
-		ret = PAPI_reset( EventSet );
-		if ( ret != PAPI_OK )
-			std::cout << "FAIL reset" << endl; 
-
-
-
 	}while (op != 0);
-
-	ret = PAPI_remove_event( EventSet, PAPI_L1_DCM );
-	if ( ret != PAPI_OK )
-		std::cout << "FAIL remove event" << endl; 
-
-	ret = PAPI_remove_event( EventSet, PAPI_L2_DCM );
-	if ( ret != PAPI_OK )
-		std::cout << "FAIL remove event" << endl; 
-
-	ret = PAPI_destroy_eventset( &EventSet );
-	if ( ret != PAPI_OK )
-		std::cout << "FAIL destroy" << endl;
-
 }
